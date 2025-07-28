3bb61f21be79d171667e50eed215f65f6efe2f55
Prince Malviya <princemalviya349@gmail.com>
2025-07-28 19:02:18 +0530

    first commit

---
+++ Jarvis/musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "geet": "https://www.youtube.com/watch?v=6vvzvrZS9zk&list=RDzccS_IIQ-QY"
+    # HHJOdxgVM6vvzvrZS9zk&start_radio=1
+}
